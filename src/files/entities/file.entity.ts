d4e4779b2458200587bb8a5baae8bbf48ccf5df9
vldmr42 <kmb14056@yandex.ru>
2023-11-11 15:07:39 +0400

    init and somme config and basic entities

---
+++ src/files/entities/file.entity.ts
@@ -0,0 +1,32 @@
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('files')
+export class FileEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  originalName: string;
+
+  @Column()
+  size: number;
+
+  @Column()
+  momeType: string;
+
+  @ManyToOne(() => UserEntity, (user) => user.files)
+  user: UserEntity;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
---
+++ src/files/storage.ts
@@ -0,0 +1,23 @@
+import { diskStorage } from 'multer';
+
+const generateId = () => {
+  Array(18)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+};
+
+const normalizeFileName = (
+  req: any,
+  file: Express.Multer.File,
+  callback: (arg0: null, arg1: string) => void,
+) => {
+  const fileExtName = file.originalname.split('.').pop();
+
+  callback(null, `${generateId()}.${fileExtName}`);
+};
+
+export const fileStorage = diskStorage({
+  destination: './uploads',
+  filename: normalizeFileName,
+});
